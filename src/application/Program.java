4e784b55d9cf0aefb2dc98e8ac2bc2eddaa065a5
Guilherme <guilherme_riba@hotmail.com>
2024-04-10 11:23:34 -0300

    Create a query to update data in DB

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,6 +9,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
@@ -78,6 +79,36 @@ public class Program {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		} finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		*/
+		
+		/*
+		 * Atualizar dados
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)"
+					);
+			
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
