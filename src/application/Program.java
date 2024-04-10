0c318ba0ac8ac0d0fe4aad9a1d91f6b7826f3af4
Guilherme <guilherme_riba@hotmail.com>
2024-04-10 11:37:46 -0300

    Create DbIntegrityException and a query to delete data in DB

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,7 @@ import java.text.SimpleDateFormat;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -107,6 +108,34 @@ public class Program {
 			System.out.println("Done! Rows affected: " + rowsAffected);
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		*/
+		
+		/*
+		 * Deletar dados
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"DELETE FROM department "
+					+ "WHERE "
+					+ "Id = ?"
+					);
+			
+			st.setInt(1, 2);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		} catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
